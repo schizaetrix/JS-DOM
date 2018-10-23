3c67a8f737be7edc9c79f1e8e9715a0dbfbb38c8
Chiza Barber <chiza.alba@gmail.com>
2018-10-23 10:59:01 -0500

    exercise 2 completed

--- ex2/script.js
+++ ex2/script.js
@@ -1,1 +1,7 @@
 // Change the background color of the box to blue
+
+var boxDiv = document.getElementById('box')
+
+document.getElementById('box').onclick = function () {
+  boxDiv.style.backgroundColor = 'blue'
+}
