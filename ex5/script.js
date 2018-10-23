da1e7316b32983ae0fabc4331fe18c95755e2809
Chiza Barber <chiza.alba@gmail.com>
2018-10-23 12:25:01 -0500

    Corrected HTML title to "Exercise5" | completed exercise 5

--- ex5/script.js
+++ ex5/script.js
@@ -1,3 +1,13 @@
+// Exercise 5
 // Add a box to "boxContainer" everytime you click the button
 // HINT: Look up createElement(), appendChild()
 // HINT HINT: You can add the "box" class to elements like so: https://www.w3schools.com/howto/howto_js_add_class.asp
+
+var button = document.getElementById('myButton')
+
+button.addEventListener('click', function () {
+  var addBox = document.createElement('div')
+  var containerBox = document.getElementById('boxContainer')
+  containerBox.appendChild(addBox)
+  addBox.classList.add('box')
+})
