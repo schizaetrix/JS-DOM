c7ffb874363395172bbb472ed67cd36b316466e0
Chiza Barber <chiza.alba@gmail.com>
2018-10-23 10:45:31 -0500

    exercise 1 completed

--- ex1/script.js
+++ ex1/script.js
@@ -1,1 +1,7 @@
 // Change the text of the h1 tag to "I have changed the text"
+
+var idH1 = document.querySelector('h1')
+
+document.querySelector('h1').onclick = function () {
+  idH1.innerHTML = 'I have changed the text'
+}
