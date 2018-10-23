9c6845ff224fd471ca97b122cabdc663119429df
Chiza Barber <chiza.alba@gmail.com>
2018-10-23 14:00:26 -0500

    exercise 6 complete

--- ex6/script.js
+++ ex6/script.js
@@ -1,14 +1,17 @@
-//Finish the code below to make the counter increment everytime you press the button
-//Also, write the listener for the decrement button so that the counter decrements when you click it
-var incrementButton = document.getElementById('increment');
-var decrementButton = document.getElementById('decrement');
-var counter = document.getElementById('counter');
+// Finish the code below to make the counter increment everytime you press the button
+// Also, write the listener for the decrement button so that the counter decrements when you click it
+var incrementButton = document.getElementById('increment')
+var decrementButton = document.getElementById('decrement')
+var counter = document.getElementById('counter')
 
-var count = 0; // This variable holds the current value of the counter!
+var count = 0 // This variable holds the current value of the counter!
 
-incrementButton.addEventListener('click', function(){
+incrementButton.addEventListener('click', function () {
+  count = count + 1
+  counter.innerHTML = count
+} )
 
-    counter.innerHTML = count;
-    
-});
-
+decrementButton.addEventListener('click', function () {
+  count = count - 1
+  counter.innerHTML = count
+} )
