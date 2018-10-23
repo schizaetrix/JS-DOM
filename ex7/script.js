3437c99723d941183fcba64e411a4ac908dca5f5
Chiza Barber <chiza.alba@gmail.com>
2018-10-23 15:13:20 -0500

    Added Bootstrap stylesheet and jquery scripts to bottom of HTML | exercise 7 complete

--- ex7/script.js
+++ ex7/script.js
@@ -1,3 +1,14 @@
 // Make the section disappear and reappear whenever you click the section-header
 // BONUS: Can you make the section slide up and down like this? :
 // https://www.w3schools.com/bootstrap/bootstrap_collapse.asp
+
+var sectionHeader = document.getElementById('section-header')
+var sectionBody = document.getElementById('section')
+
+sectionHeader.addEventListener('click', function () {
+  if (sectionBody.style.display === 'none') {
+    sectionBody.style.display = 'block'
+  } else {
+    sectionBody.style.display = 'none'
+  }
+} )
